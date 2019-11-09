dde0ad3a3fa527832f67283e98c741c1b1415f9c
EC2 Default User <ec2-user@ip-172-31-38-83.us-east-2.compute.internal>
2019-11-09 04:19:15 +0000

    model copy

---
+++ app/Equipments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Equipments extends Model
+{
+    use SoftDeletes;
+    
+    // aaaa
+}
